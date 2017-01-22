04dd989b10a682b62aeac631361d2e525fa81327
Shawn S Hillyer <shawn.hillyer@gmail.com>
2017-01-21 20:44:55 -0800

    Extend Room class in room.py, add RoomFeature and RoomConnection classes

--- fileio/room_builder.py
+++ fileio/room_builder.py
@@ -1,4 +1,4 @@
-from gameclient.room import Room
+from gameclient.room import *
 
 from debug.debug import *
 logger = logging.getLogger(__name__)
@@ -10,8 +10,55 @@ class RoomBuilder:
         logger.debug("RoomBuilder instantiated")
 
     def load_room_data_from_file(self):
-        rooms = []
+        # rooms = []
+        # room_properties = { 'to' : 'do '}
+        # rooms.append(Room(room_properties))
 
-        rooms.append(Room())
+        # return rooms
+
+        return self.build_rooms_from_code()
+
+    def build_rooms_from_code(self):
+        '''
+        Method for use in testing game engine and demonstrates how the features and rooms could be parsed
+        from a file; would be useful if we were hard-coding the values. Could also convert to the real
+        file loader by replacing static strings with information from various file-read operations
+        :return:
+        '''
+        street_feature_1_properties = {
+            'name' : '"No Skateboarding" sign',
+            'description' : 'The sign states very clearing that skateboarding is prohibited in this area. Fine: $300',
+            'known_to_player' : False
+        }
+        street_feature_2_properties = {
+            'name' : 'Guardrails',
+            'description' : 'These guard rails look perfect for grinding on with a skateboard!',
+            'known_to_player' : False
+        }
+
+        street_connection_1_properties = {
+                'label': 'Arcade',
+                'cardinal_direction': 'North',
+                'description': "an exciting sign for an arcade",
+                'destination': 'Arcade'
+        }
+
+
+        street_feature_1 = RoomFeature(street_feature_1_properties)
+        street_feature_2 = RoomFeature(street_feature_2_properties)
+        street_connection_1 = RoomConnection(street_connection_1_properties)
+
+        street_properties = {
+            'room_features' : [street_feature_1, street_feature_2],
+            'long_description' : 'This is a really long description of the Street and would be read from a file in the event of a real program',
+            'short_description' : "You're standing on the street",
+            'visited' : False,
+            'room_connections' : [street_connection_1]
+        }
+
+
+        street = Room(street_properties)
+
+        rooms =  [ street ]
 
         return rooms
--- gameclient/room.py
+++ gameclient/room.py
@@ -4,6 +4,52 @@ logger = logging.getLogger(__name__)
 
 
 class Room:
-    def __init__(self):
+    def __init__(self, properties):
         logger.debug("Room initialized")
+        if properties:
+            self.room_features = properties['room_features']
+            self.long_description = properties['long_description']
+            self.short_description = properties['short_description']
+            self.visited = properties['visited']
+            self.room_connections = properties['room_connections']
 
+    def get_long_description(self):
+        full_description = self.long_description
+        if self.room_connections:
+            for connection in self.room_connections:
+                full_description = full_description + connection.get_connection_description()
+
+
+class RoomFeature:
+    '''
+    A feature of a room. Each room has a minimum of two features that can be examined
+    Examining a feature causes it to be known_to_player
+    '''
+    def __init__(self, properties):
+        self.name = properties['name']
+        self.description = properties['description']
+        self.known_to_player = properties['known_to_player']
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def is_known_to_player(self):
+        return self.known_to_player
+
+    def discover(self):
+        self.known_to_player = True
+
+
+class RoomConnection:
+    def __init__(self, properties):
+        self.label = properties['label']
+        self.cardinal_direction = properties['cardinal_direction']
+        self.description = properties['description']
+        self.destination = properties['destination']
+
+    def get_connection_description(self):
+        description = "To the " + self.cardinal_direction + " you see " + self.description + "."
+        return description
