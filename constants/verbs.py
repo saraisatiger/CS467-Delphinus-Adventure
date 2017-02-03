4839216ef724a7aadfe8fe558b12efab336edb2f
Niza Volair <volairn@flip1.engr.oregonstate.edu>
2017-02-03 01:08:15 -0800

    add keywords, v-s-p-t parser logic, room 16 description

--- constants/verbs.py
+++ constants/verbs.py
@@ -9,42 +9,96 @@
 #
 # CITATIONS
 # CITE: None
+#Additions of subjects & prepositions add to language parser functionality, though they don't do much for the file name -NV
+#subjects- objects and features
+#objects and aliases
+CASH_CRISP = 'crisp cash'
+CASH_CRISP_ALIASES = {'crisp', 'crisp money'} 
+CASH_WAD = 'cash wad'
+CASH_WAD_ALIASES = {'wad of cash', 'wad of money', 'money wad'}
+SKATEBOARD = 'skateboard'
+SKATEBORD_ALIASES = {'board', 'plank'}
+HACKERSNACKS = 'hackersnacks'
+HACKERSNACKS_ALIASES = {'snacks', 'munchies'}
+SURGE = 'surge'
+SURGE_ALIASES = {'soda', 'drink', 'beverage'} 
+SPRAY_PAINT = 'spray paint'
+SPRAY_PAINT_ALIASES = {'paint'}
+FLOPPY_DISK = 'floppy disk'
+FLOPPY_DISK_ALIASES = {'floppy', 'disk'}
+GRAPHICS_CARD = 'graphics card'
+GRAPHICS_CARD_ALIASES = {'card', 'graphics'}
+RAM = 'ram chip'
+RAM_ALIASES = {'random access memory', 'memory', 'chip', 'ram'}
 
+OBJECTS = [SKATEBOARD, HACKERSNACKS, SURGE, SPRAY_PAINT, FLOPPY_DISK, GRAPHICS_CARD, RAM]
+
+OBJECTS_AND_ALIASES = [SKATEBOARD, SKATEBORD_ALIASES, HACKERSNACKS, HACKERSNACKS_ALIASES, SURGE, SURGE_ALIASES, SPRAY_PAINT, SPRAY_PAINT_ALIASES, FLOPPY_DISK, FLOPPY_DISK_ALIASES, GRAPHICS_CARD, GRAPHICS_CARD_ALIASES, RAM, RAM_ALIASES]
+
+#features 
+#TODO: add aliases, simple 'look at' features and descriptions to ROOMS, then add logic here
+R1_F = {'no skateboarding sign', 'gaurdrails'}
+R2_F = {'phone booth', 'trash can'}
+R3_F = {'ramp', 'death to the patriarchy'}
+R4_F = {'counter', 'shelves', 'store clerk'}
+R5_F = {'atm', 'cash', 'guard'}
+R6_F = {'your gear', 'unattended police computer', 'police officer', 'unoccupied desk', 'metal door'}
+R7_F = {'locker', 'teacher', 'fire alarm'}
+R8_F = {'school-wide intercom mic', 'office computer', 'acid burn', 'clock', 'poster'}
+R9_F = {'door', 'ledge'}
+R10_F = {'disk drive', 'panel', 'terminal'}
+R11_F = {'bug', 'firewall'}
+R12_F = {'emojis', 'creature', 'acid burn'}
+R13_F = {'input / output port', 'sentient cpu'}
+R14_F = {'cat videos from the internet', 'nuclear launch codes'}
+R15_F = {'binary files', 'corrupted files'}
+R16_F = {'acid burn', 'diving board', 'windows'}
+
+FEATURES = [R1_F, R2_F, R3_F, R4_F, R5_F, R6_F, R7_F, R8_F, R9_F, R10_F, R11_F, R12_F, R13_F, R14_F, R15_F, R16_F]
+
+#propositions
+PREPOSITIONS = {'on', 'in', 'onto', 'into', 'below', 'behind', 'above', 'over', 'next to', 'in front of', 'about'}
+
+#verbs
 # In alphabetical order
 BUY = 'buy'
-BUY_ALIASES = {'buy', 'purchase'}
+BUY_ALIASES = {'buy', 'purchase', 'pay for'}
 CHEATCODE_LOSE = 'cheatcode lose'
 CHEATCODE_LOSE_ALIASES = {'mess with the best'}
 CHEATCODE_WIN = 'cheatcode win'
 CHEATCODE_WIN_ALIASES = {'die like the rest'}
 DROP = 'drop'
-DROP_ALIASES = {'drop', 'put down', 'let go'}
+DROP_ALIASES = {'drop', 'put down', 'let go', 'leave'}
 GO = "go"
-GO_ALIASES = {'go'}
+GO_ALIASES = {'go', 'move', 'walk', 'run'}
 HACK = 'hack'
 HACK_ALIASES = {'hack'}
 HELP = 'help'
-HELP_ALIASES = {'help'}
+HELP_ALIASES = {'help', 'info', 'information', 'assist', 'assistance'}
 LOAD_GAME = 'load game'
-LOAD_GAME_ALIASES = {'loadgame', 'load game', 'load_game_menu'}
+LOAD_GAME_ALIASES = {'loadgame', 'load game', 'load_game_menu', 'load my game', 'load saved game', 'load old game'}
 LOOK = 'look'
 LOOK_ALIASES = {'look'}
 LOOK_AT = 'look at'
 LOOK_AT_ALIASES = {'look at', 'examine'}
 TAKE = 'take'
-TAKE_ALIASES = {'take', 'pick up'}
+TAKE_ALIASES = {'take', 'pick up', 'grab', 'acquire'}
 INVENTORY = 'inventory'
 INVENTORY_ALIASES = {'inventory', 'backpack', 'bag'}
 INVALID_INPUT = 'invalid command'
 NEW_GAME = 'new game'
 NEW_GAME_ALIASES = {'newgame', 'new game', "hacktheplanet"}
 QUIT = 'quit'
-QUIT_ALIASES = {'quit', 'exit', 'bye'}
+QUIT_ALIASES = {'quit', 'exit', 'bye', 'goodbye', 'leave game'}
 SAVE_GAME = 'save game'
-SAVE_GAME_ALIASES = ['savegame', 'save game', 'save_game']
+SAVE_GAME_ALIASES = ['save', 'savegame', 'save game', 'save_game']
 SPRAYPAINT = 'spraypaint'
 SPRAYPAINT_ALIASES = {'spraypaint', 'tag', 'spray'}
 STEAL = 'steal'
 STEAL_ALIASES = {'steal'}
 USE = 'use'
 USE_ALIASES = {'use'}
+
+VERBS = [BUY, CHEATCODE_LOSE, CHEATCODE_WIN, DROP, GO, HACK, HELP, LOAD_GAME, LOOK, LOOK_AT, TAKE, INVENTORY,  NEW_GAME, QUIT, SAVE_GAME, SPRAYPAINT, STEAL, USE]
+
+VERB_AND_ALIASES = [BUY, BUY_ALIASES, CHEATCODE_LOSE, CHEATCODE_LOSE_ALIASES, CHEATCODE_WIN, CHEATCODE_WIN_ALIASES, DROP, DROP_ALIASES, GO, GO_ALIASES, HACK, HACK_ALIASES, HELP, HELP_ALIASES, LOAD_GAME, LOAD_GAME_ALIASES, LOOK, LOOK_ALIASES, LOOK_AT, LOOK_AT_ALIASES, TAKE, TAKE_ALIASES, INVENTORY, INVENTORY_ALIASES, NEW_GAME, NEW_GAME_ALIASES, QUIT, QUIT_ALIASES, SAVE_GAME, SAVE_GAME_ALIASES, SPRAYPAINT, SPRAYPAINT_ALIASES, STEAL, STEAL_ALIASES, USE, USE_ALIASES]
--- languageparser/language_parser.py
+++ languageparser/language_parser.py
@@ -18,11 +18,11 @@
 # be interacted. For example "use broom on dusty floor" might return:
 #
 # {
-#     'verb' : 'use',
-#     'verb_object' : 'broom',
-#     'targets' : [
-#         'dusty floor'
-#     ]
+#	 'verb' : 'use',
+#	 'verb_object' : 'broom',
+#	 'targets' : [
+#		 'dusty floor'
+#	 ]
 # }
 #
 # (SSH)
@@ -38,102 +38,198 @@ logger = logging.getLogger(__name__)
 
 
 class LanguageParser:
-    '''
-        TODO:
+	'''
+		TODO:
 
-    '''
-    def __init__(self):
-        logger.debug("Language Parser initialized")
-
-
-    def parse_command(self, command):
-        '''
-        Presently returning a constant defined in a constants/verbs.py file so that the
-        return values from parser can just be set. Thi is Shawn's temporary solution. Later on
-        we will need to send more than just the verb back (the subject etc. also needed). See Dev note near file head
-        '''
-
-        # Parse any command to all lowercase to reduce complexity of our parser.
-        # TODO: Might also want to strip trailing whitespace (SSH)
-        # l.strip() strips the left-side whitespace, not sure on right side whitespace (SSH)
-        command = command.lower().lstrip()
-        object = None
-        targets = None
-
-        # Hacky way to parse a "look at" command to find the verb_object/feature player wants to examine.
-        # NOTE: Doesn't parse aliases
-        if 'look at' in command:
-            object = command.replace("look at ", "", 1) # replace "look at " with empty string - rest is the verb_object
-            command = "look at"
-
-        # hacky way to parse a 'take' command.
-        # NOTE: Doesn't parse aliases
-        elif 'take' in command:
-            object = command.replace("take ", "", 1) # replace at most one instance of "take " with empty str
-            command = "take"
-
-        # hacky way to parse a 'drop' command
-        # NOTE: Doesn't parse aliases
-        elif 'drop' in command:
-            object = command.replace("drop ", "", 1) # replace at most one instance of "drop " with empty str
-            command = "drop"
-
-        # Same thing for go
-        elif 'go' in command:
-            object = command.replace("go ", "", 1)
-            command = "go"
-
-        elif 'buy' in command:
-            object = command.replace("buy ", "", 1)
-            command = "buy"
-
-        elif 'use' in command:
-            object = command.replace("use ", "", 1)
-            command = "use"
+	'''
+	def __init__(self):
+		logger.debug("Language Parser initialized")
 
 
-        # This simple code just checks if the string entered by user us in one of several Lists defined in the resource
-        # file constants/verbs.py. Each list is a set of aliases for each verb and it returns a simple string that
-        # the gameclient is able to examine.
-        # Once this is re-implemented in a stable way, gameclient will need to be reconfigured to properly parse whatever
-        # ends up being returned by the parser. -- (SSH)
+	def parse_command(self, command):
+		'''
+		Presently returning a constant defined in a constants/verbs.py file so that the
+		return values from parser can just be set. Thi is Shawn's temporary solution. Later on
+		we will need to send more than just the verb back (the subject etc. also needed). See Dev note near file head
+		'''
 
-        if command in QUIT_ALIASES:
-            command = QUIT
-        elif command in NEW_GAME_ALIASES:
-            command = NEW_GAME
-        elif command in LOAD_GAME_ALIASES:
-            command = LOAD_GAME
-        elif command in SAVE_GAME_ALIASES:
-            command = SAVE_GAME
-        elif command in HELP_ALIASES:
-            command = HELP
-        elif command in LOOK_ALIASES:
-            command = LOOK
-        elif command in LOOK_AT_ALIASES:
-            command = LOOK_AT
-        elif command in GO_ALIASES:
-            command = GO
-        elif command in TAKE_ALIASES:
-            command = TAKE
-        elif command in DROP_ALIASES:
-            command = DROP
-        elif command in INVENTORY_ALIASES:
-            command = INVENTORY
-        elif command in BUY_ALIASES:
-            command = BUY
-        elif command in USE_ALIASES:
-            command = USE
-        elif command in SPRAYPAINT_ALIASES:
-            command = SPRAYPAINT
-        # cheat codes
-        elif command == "mess with the best":
-            command = CHEATCODE_LOSE
-        elif command == "die like the rest":
-            command = CHEATCODE_WIN
+		# Parse any command to all lowercase to reduce complexity of our parser.
+		# TODO: Might also want to strip trailing whitespace (SSH)
+		# l.strip() strips the left-side whitespace, not sure on right side whitespace (SSH)
+		command = command.lower().lstrip()
+		object = None
+		targets = None
+		
+		# New Logic -NV
+		# parse command into words
+		words = command.split()
+		
+		# Go through list of words and try to pull out each significant word in VERB-SUBJECT-PREPOSTION-TARGET order 
+		# Note: we are using 'TARGET' to mean 'object of the preposition' to avoid confusion with objects
+		# STRETCH GOAL: account for NEGATIONS
+		# STRETCH GOAL: account for other word sentence structure
+		# STRETCH GOAL: improve logic of for loops if possible to reduce time
+		verb = None
+		subject = None
+		subject_is = None
+		preposition = None
+		target = None
+		target_is = None
+		cur_index = 0
+		save_cur_index = 0
 
-        else:
-            command = INVALID_INPUT
+		# Add Aliases logic .eval()?
+		
+		# First significant word should be the VERB
+		i = 0
+		while i < len(words):
+			if words[i] in VERBS:
+				cur_index = i + 1
+				verb = words[i]
+				break
+			i += 1
+						
+		# Second significant word should be the SUBJECT (optional) either a FEATURE or OBJECT
+		i = cur_index
+		save_cur_index = cur_index
+		while i < len(words):
+			if words[i] in FEATURES:
+				cur_index = i + 1
+				subject = words[i]
+				subject_is = 'FEATURE'
+				break
+			i += 1
+						
+		if subject_is != 'FEATURE':
+			i = save_cur_index
+			while i < len(words):
+				if words[i] in OBJECTS:
+					cur_index = i + 1
+					subject = words[i]
+					subject_is = 'OBJECT'
+					break
+				i += 1
+							
+		# If there is no VERB, command is invalid
+		if verb == None:
+			verb = INVALID_INPUT
+			
+		# Third significant word should be the PREPOSITION (optional)
+		# If there is no SUBJECT, skip the check for PREPOSTION
+		if subject != None:
+			i = cur_index
+			while i < len(words):
+				if words[i] in PREPOSITIONS:
+					cur_index = i + 1
+					preposition = words[i]
+					break
+				i += 1
+							
+		# Fourth significatn word should be the TARGET (optional)either a FEATURE or OBJECT
+		# If there is no PREPOSTION, skip the check for TARGET 
+		save_cur_index = cur_index
+		if subject != None and preposition != None:
+			i = cur_index
+			while i < len(words):
+				if words[i] in FEATURES:
+					cur_index = i + 1
+					target = words[i]
+					target_is = 'FEATURE'
+					break
+				i += 1
+						
+		if subject != None and preposition != None and target_is != 'FEATURE':
+			#reset to same starting search index as before
+			i = save_cur_index
+			while i < len(words):
+				if words[i] in OBJECTS:
+					cur_index = i + 1
+					target = words[i]
+					target_is = 'OBJECT'
+					break
+							
+		logger.debug("New Logic: " + str(verb) + ", " + str(subject) + ", " + str(preposition)+ ", " + str(target))
+		# Put in JSON Object
+		# Return 
+		
+		
+		#END New logic
+		
 
-        logger.debug("Returning: " + str(command) + ", " + str(object) + ", " + str(targets))
-        return (command, object, targets)
+		# Hacky way to parse a "look at" command to find the verb_object/feature player wants to examine.
+		# NOTE: Doesn't parse aliases
+		if 'look at' in command:
+			object = command.replace("look at ", "", 1) # replace "look at " with empty string - rest is the verb_object
+			command = "look at"
+
+		# hacky way to parse a 'take' command.
+		# NOTE: Doesn't parse aliases
+		elif 'take' in command:
+			object = command.replace("take ", "", 1) # replace at most one instance of "take " with empty str
+			command = "take"
+
+		# hacky way to parse a 'drop' command
+		# NOTE: Doesn't parse aliases
+		elif 'drop' in command:
+			object = command.replace("drop ", "", 1) # replace at most one instance of "drop " with empty str
+			command = "drop"
+
+		# Same thing for go
+		elif 'go' in command:
+			object = command.replace("go ", "", 1)
+			command = "go"
+
+		elif 'buy' in command:
+			object = command.replace("buy ", "", 1)
+			command = "buy"
+
+		elif 'use' in command:
+			object = command.replace("use ", "", 1)
+			command = "use"
+
+
+		# This simple code just checks if the string entered by user us in one of several Lists defined in the resource
+		# file constants/verbs.py. Each list is a set of aliases for each verb and it returns a simple string that
+		# the gameclient is able to examine.
+		# Once this is re-implemented in a stable way, gameclient will need to be reconfigured to properly parse whatever
+		# ends up being returned by the parser. -- (SSH)
+
+		if command in QUIT_ALIASES:
+			command = QUIT
+		elif command in NEW_GAME_ALIASES:
+			command = NEW_GAME
+		elif command in LOAD_GAME_ALIASES:
+			command = LOAD_GAME
+		elif command in SAVE_GAME_ALIASES:
+			command = SAVE_GAME
+		elif command in HELP_ALIASES:
+			command = HELP
+		elif command in LOOK_ALIASES:
+			command = LOOK
+		elif command in LOOK_AT_ALIASES:
+			command = LOOK_AT
+		elif command in GO_ALIASES:
+			command = GO
+		elif command in TAKE_ALIASES:
+			command = TAKE
+		elif command in DROP_ALIASES:
+			command = DROP
+		elif command in INVENTORY_ALIASES:
+			command = INVENTORY
+		elif command in BUY_ALIASES:
+			command = BUY
+		elif command in USE_ALIASES:
+			command = USE
+		elif command in SPRAYPAINT_ALIASES:
+			command = SPRAYPAINT
+		# cheat codes
+		elif command == "mess with the best":
+			command = CHEATCODE_LOSE
+		elif command == "die like the rest":
+			command = CHEATCODE_WIN
+
+		else:
+			command = INVALID_INPUT
+
+		logger.debug("Returning: " + str(command) + ", " + str(object) + ", " + str(targets))
+		return (command, object, targets)
