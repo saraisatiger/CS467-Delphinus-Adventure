6f4e10224fc3a3fd984ac30d621e9556e8b65a6e
Shawn S Hillyer <shawn.hillyer@gmail.com>
2017-02-24 11:03:54 -0800

    Add verb_skate, comment non-obvious functions in gamestate/client files

--- gameclient/game_state.py
+++ gameclient/game_state.py
@@ -114,16 +114,16 @@ class GameState:
                 obj = self.get_object_by_name(object_name)
                 room = self.get_room_by_name(room_name)
                 room.add_object_to_room(obj)
-                logger.debug("Adding object " + obj.get_name() + " to room " + room.get_name() + ".")
-
+                # logger.debug("Adding object " + obj.get_name() + " to room " + room.get_name() + ".")
+        #
         player_inventory_list = save_data.get_player_inventory()
         for object_name in player_inventory_list:
             obj = self.get_object_by_name(object_name)
             self.player.add_object_to_inventory(obj)
-            logger.debug("Adding object " + obj.get_name() + " to player's bag.")
+            # logger.debug("Adding object " + obj.get_name() + " to player's bag.")
 
         # Player variables
-        logger.debug("self.player_cash = save_data.get_player_cash() = " + str(save_data.get_player_cash()))
+        # logger.debug("self.player_cash = save_data.get_player_cash() = " + str(save_data.get_player_cash()))
         self.player.cash = save_data.get_player_cash()
         self.player.coolness = save_data.get_player_coolness()
         self.player.speed = save_data.get_player_speed()
@@ -159,14 +159,25 @@ class GameState:
         return header_info
 
     def set_object_vars_to_default(self):
+        '''
+        Sets ownership on all objects to be False, called during new game init.
+        '''
         for obj in self.objects:
             obj.set_is_owned_by_player(False)
 
     def set_default_room_by_name(self, room_name):
+        '''
+        Sets the default room to the room_name passed in. Called during game initialization
+        :param room_name:
+        '''
         default_room = self.get_room_by_name(room_name)
         self.set_current_room(default_room)
 
     def initialize_spraypaint_data(self):
+        '''
+        Creates a dictionary of spraypaint data for every non-virtual room in the game and sets is_spraypainted false,
+        message is None
+        '''
         self.spraypaint_data.clear()
         for room in self.rooms:
             if room.is_virtual_space() is False:
@@ -177,15 +188,14 @@ class GameState:
                          'spraypaint_message': None
                 }
                 self.spraypaint_data[room_name] = entry
-
-        logger.debug(str(self.spraypaint_data)) # TODO: Comment this out later
-
-    def set_spraypaint_data(self, spraypaint_data):
-        # TODO: Implement this
-
-        pass
+        # logger.debug(str(self.spraypaint_data)) # TODO: Comment this out later
 
     def is_room_spray_painted_by_name(self, room_name):
+        '''
+        Check if a room is spraypainted by looking it up by name. Returns True or False
+        :param room_name: String, the name of the room as found in its JSON file.
+        :return: True if room is spraypainted, false otherwise
+        '''
         is_painted = None
         try:
             entry = self.spraypaint_data[room_name]
@@ -193,39 +203,60 @@ class GameState:
                 is_painted = entry['is_spraypainted']
         except:
             logger.debug("There was a problem finding the room '" + str(room_name) + "' + in the self.spraypaint_data dictionary")
-        logger.debug("is_room_spray_painted_by_name(" + room_name + ") returns " + str(is_painted))
+        # logger.debug("is_room_spray_painted_by_name(" + room_name + ") returns " + str(is_painted))
         return is_painted
 
     def set_room_spray_painted_by_name(self, room_name, painted=True):
+        '''
+        Updates the self.spraypaint_data dictionary to show that the room is painted
+        :param room_name: String. Name of the room to set the value for, as found in its JSON file.
+        :param painted:  Boolean. True if room is painted (default), False if it's not.
+        '''
         try:
             entry = self.spraypaint_data[room_name]
             entry['is_spraypainted'] = painted
-            logger.debug(str(entry))
+            # logger.debug(str(entry))
         except:
             logger.debug("Tried to set_room_spray_painted_by_name( +" + room_name + "," + str(painted) +  " ) but failed with exception.")
 
     def set_room_spray_painted_message(self, room_name, message):
+        '''
+        Set the message that is painted in the room
+        :param room_name: String. Name of the room to paint as found in its JSON file.
+        :param message:  String. The message to be printed in the room as graffiti/spraypaint.
+        :return:
+        '''
         try:
             entry = self.spraypaint_data[room_name]
             entry['spraypaint_message'] = str(message)
-            logger.debug("Setting the message... entry is now " + str(entry))
-            logger.debug(str(self.spraypaint_data))
+            # logger.debug("Setting the message... entry is now " + str(entry))
+            # logger.debug(str(self.spraypaint_data))
         except:
             logger.debug("Error setting the message in this room")
             logger.debug(str(self.spraypaint_data))
 
     def get_room_spray_painted_message(self, room_name):
+        '''
+        Looks up the message stored in the self.spraypaint_data dictionary for the room_name passed in
+        :param room_name:  String. Name of the room as found in its JSON file.
+        :return:
+        '''
         try:
-            logger.debug("Trying to get spray painted message from room " + room_name)
+            # logger.debug("Trying to get spray painted message from room " + room_name)
             entry = self.spraypaint_data[room_name]
-            logger.debug("self.spraypaint_data[" + room_name + "]" + str(entry))
-            logger.debug(entry['spraypaint_message'])
+            # logger.debug("self.spraypaint_data[" + room_name + "]" + str(entry))
+            # logger.debug(entry['spraypaint_message'])
             return entry['spraypaint_message']
         except:
-            logger.debug("Couldn't find a message for the room " + room_name + ", this isn't necessarily a problem if in a virtual space")
+            # logger.debug("Couldn't find a message for the room " + room_name + ", this isn't necessarily a problem if in a virtual space")
             return None
 
     def place_objects_in_rooms(self, game_objects):
+        '''
+        Iterates through a list of game objects and places them in the inventory or room as appropriate
+        :param game_objects:
+        :return:
+        '''
         for game_object in game_objects:
             # Cash wad is "hidden" in the trash can so you won't see it in the room in the normal fashion.
             object_location = game_object.get_default_location_name().lower()
@@ -257,7 +288,7 @@ class GameState:
     def update_time_left(self, time_change):
         '''
         Update the amount of time_left left.
-        :param time_change: Positive --> Increases time_left available. Negative --> Decreases time_left available
+        :param time_change: Integer. Positive increases time_left, Negative decreases time_left
         :return: N/A
         '''
         # TODO: Game design decision. What exactly does speed do? This implementation just adds the speed to any negative
@@ -265,6 +296,7 @@ class GameState:
         # We could also make speed some kind of multiplier or some other method
         if time_change < 0:
             time_change += self.player.speed
+            # IF player is so fast that the action would cost no time, make sure we don't change time_left
             if time_change > 0:
                 time_change = 0
         self.time_left += time_change
